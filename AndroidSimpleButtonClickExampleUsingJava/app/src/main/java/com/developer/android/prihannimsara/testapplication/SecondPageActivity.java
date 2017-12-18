e6548109c1030be3775c9b0577bb465771c9a59d
Prihan Nimsara <iamprihan@gmail.com>
2017-12-18 16:15:58 +0530

    final project

--- AndroidSimpleButtonClickExampleUsingJava/app/src/main/java/com/developer/android/prihannimsara/testapplication/SecondPageActivity.java
+++ AndroidSimpleButtonClickExampleUsingJava/app/src/main/java/com/developer/android/prihannimsara/testapplication/SecondPageActivity.java
@@ -6,11 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class SecondPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_page);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.button_submit,R.id.button_submit2})
+    public void submit(View view) {
+        Intent intent = new Intent(getApplicationContext(),FirstPageActivity.class);
+        startActivity(intent);
     }
 }
